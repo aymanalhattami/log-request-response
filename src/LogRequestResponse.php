35a708f967af204062bc53a26743b93f05aeb9b6
Ayman Alhattami <ayman.m.alhattami@gmail.com>
2023-05-30 10:01:12 +0300

    feat: prepare package skeleton

---
+++ src/LogRequestResponse.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Aymanalhattami\LogRequestResponse;
+
+class LogRequestResponse
+{
+    // Build your next great package.
+}
---
+++ src/LogRequestResponseServiceProvider.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Aymanalhattami\LogRequestResponse;
+
+use Illuminate\Support\ServiceProvider;
+
+class LogRequestResponseServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     */
+    public function boot()
+    {
+        /*
+         * Optional methods to load your package assets
+         */
+        // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'log-request-response');
+        // $this->loadViewsFrom(__DIR__.'/../resources/views', 'log-request-response');
+        // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        // $this->loadRoutesFrom(__DIR__.'/routes.php');
+
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__.'/../config/config.php' => config_path('log-request-response.php'),
+            ], 'config');
+
+            // Publishing the views.
+            /*$this->publishes([
+                __DIR__.'/../resources/views' => resource_path('views/vendor/log-request-response'),
+            ], 'views');*/
+
+            // Publishing assets.
+            /*$this->publishes([
+                __DIR__.'/../resources/assets' => public_path('vendor/log-request-response'),
+            ], 'assets');*/
+
+            // Publishing the translation files.
+            /*$this->publishes([
+                __DIR__.'/../resources/lang' => resource_path('lang/vendor/log-request-response'),
+            ], 'lang');*/
+
+            // Registering package commands.
+            // $this->commands([]);
+        }
+    }
+
+    /**
+     * Register the application services.
+     */
+    public function register()
+    {
+        // Automatically apply the package configuration
+        $this->mergeConfigFrom(__DIR__.'/../config/log-request-response.php', 'log-request-response');
+
+        // Register the main class to use with the facade
+        // $this->app->singleton('log-request-response', function () {
+        //     return new LogRequestResponse;
+        // });
+    }
+}
