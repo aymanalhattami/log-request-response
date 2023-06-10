280e61bdecd1a27c394e4d537e38df999d70e343
Ayman Alhattami <ayman.m.alhattami@gmail.com>
2023-06-10 22:40:42 +0300

    refactor: in middleware

--- config/log-request-response.php
+++ config/log-request-response.php
@@ -4,5 +4,26 @@
  * You can place your custom package configuration in here.
  */
 return [
+    'enabled' => true,
+    'log_level' => 'info',
+    'log_channel' => env('LOG_REQUEST_RESPONSE_CHANNEL', 'daily'),
 
+    'enable_log_auth' => true,
+    'enable_log_request' => true,
+    'enable_log_response' => true,
+    'enable_log_header' => true,
+    'enable_log_url' => true,
+    'enable_log_method' => true,
+    'enable_log_ip' => true,
+    'enable_log_duration' => true,
+
+    'columns' => [
+        'only' => [],
+        'except' => []
+    ],
+
+    'urls' => [
+        'only' => [],
+        'except' => [],
+    ]
 ];
--- src/Http/Middleware/LogRequestResponseMiddleware.php
+++ src/Http/Middleware/LogRequestResponseMiddleware.php
@@ -4,8 +4,13 @@ namespace AymanAlhattami\LogRequestResponse\Http\Middleware;
 
 use Closure;
 use Illuminate\Support\Arr;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Log;
+
+use function PHPSTORM_META\type;
+use function Psy\debug;
 
 class LogRequestResponseMiddleware
 {
@@ -23,24 +28,48 @@ class LogRequestResponseMiddleware
 
     protected function log($request, $response)
     {
-        $duration = $request->end - $request->start;
-        $url = $request->fullUrl();
-        $method = strtoupper($request->getMethod());
-        $headers = $request->headers->all();
-        $ip = $request->getClientIp();
-
-        $requestedData = $request->all();
-        $response = $response->getContent();
-
-        $array = Arr::dot($requestedData);
-
-        Log::info($url, [
-            "Request"   => Arr::except($requestedData, ['identity_image1', 'identity_image2', 'selfie']),
-            "Response"  => json_decode($response, JSON_UNESCAPED_UNICODE),
-            "ip"        => $ip,
-            "method"    => $method,
-            "Duration"  => $duration . 'ms',
-            "Headers"   => Arr::dot(collect($headers)),
+        Log::info('Log request and response', [
+            "request" => Arr::except($request->all(), ['identity_image1', 'identity_image2', 'selfie']),
+            "response" => $this->getResponse($response),
+            "header" => $this->getHeaders($request),
+            'url' => $this->getUrl($request),
+            "ip" => $this->getIp($request),
+            "method" => $this->getMethod($request),
+            "duration" => $this->getDuration($request),
         ]);
+    }
+
+    private function getDuration($request): string
+    {
+        return ($request->end - $request->start) . 'ms';
+    }
+
+    private function getUrl($request): string
+    {
+        return $request->fullUrl();
+    }
+
+    private function getMethod($request): string
+    {
+        return Str::of($request->getMethod())->upper();
+    }
+
+    private function getHeaders($request): array
+    {
+        return $request->headers->all();
+    }
+
+    private function getIp($request): string
+    {
+        return $request->getClientIp();
+    }
+
+    private function getResponse($response)
+    {
+        if (!Str::of($response->getContent())->isJson()) {
+            return $response->getContent();
+        } else {
+            return json_decode($response->getContent(), JSON_UNESCAPED_UNICODE);
+        }
     }
 }
